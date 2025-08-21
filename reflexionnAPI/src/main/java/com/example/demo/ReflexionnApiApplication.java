d35657a92a17a35e3555bc89e046a3c0ecb79ba0
Nawed Khan <hp@DESKTOP-HJ5R41N>
2025-08-21 14:05:01 +0530

    nawed changes in git 1

--- reflexionnAPI/src/main/java/com/example/demo/ReflexionnApiApplication.java
+++ reflexionnAPI/src/main/java/com/example/demo/ReflexionnApiApplication.java
@@ -8,6 +8,7 @@ public class ReflexionnApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReflexionnApiApplication.class, args);
+		System.out.println("nawed");
 	}
 
 }
